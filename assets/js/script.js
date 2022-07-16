05ce339a3376ad6204c06ea53d8b26cfe83260c4
Erik1007 <erik.hartman1007@gmail.com>
2022-07-16 09:20:55 +0000

    add basic outline functions and structure and winners rule to JS page

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+
+
+  const winrule = {
+    Spock : ["Scissor", "Rock"],
+
+    Rock : [ "Lizzard", "Scissor"], 
+
+    Lizard : ["Spock", "Paper"], 
+
+    Scissor : ["Paper", "Lizzard"], 
+
+    Paper : ["Rock", "Spock"],
+  };
+
+  function runGame() {
+
+  }
+
+  function checkAnswer() {
+
+  }
+
+  function incrementPlayerScore() {
+
+  }
+
+  function incrementComputerScore() {
+
+  }
+
+  function reset() {
+
+  }
