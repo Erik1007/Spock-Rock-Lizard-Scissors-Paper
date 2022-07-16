447147b461a4f6601d62b0affba86edfa2e7bfa9
Erik1007 <erik.hartman1007@gmail.com>
2022-07-16 10:26:02 +0000

    added first event listeners for click/ run game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
   const winrule = {
     Spock : ["Scissor", "Rock"],
@@ -11,6 +12,21 @@
 
     Paper : ["Rock", "Spock"],
   };
+
+  document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                checkAnswer();
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        });
+    }
+});
 
   function runGame() {
 
