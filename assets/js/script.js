303516f1a1552ac76c85b08d2e443325e1a5d904
Erik1007 <erik.hartman1007@gmail.com>
2022-07-18 21:19:53 +0000

    attempting to troubleshoot a bugged script. all script was removed and single lines have been re inserted one by one to locate issue with code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-let options = [{
+let buttons = document.querySelectorAll('.options');
+  for (let button of buttons) {
+    button.addEventListener("click", function() {
+      document.getElementsByClassName("option"); {
+        if (this.getAttribute("option") === "submit") {
+          checkAnswer();
+          alert("buttong clicked");
+        } 
+        
+      }
+    }) 
+  };
+
+  let options = [{
     image: "lizard.webp",
     name: "Lizard",
   },
@@ -21,125 +34,16 @@ let options = [{
     image: "spock.webp",
     name: "Spock",
   },
-];
+];      
+console.log(options);
 
 
-function runGame() {
-  let pChoice = document.getElementsByClassName("options");
-  let options = Array.from(document.getElementsByClassName("players"));
 
-  options.forEach((option) => {
-    option.addEventListener("click", function () {
-      pChoice = this.textContent;
-      console.log(pChoice)
+  reset.addEventListener("click", function () {
 
-      let cChoice = ["Spock", "Rock", "Lizzard", "Scissors", "Paper"];
-      cChoice = options[Math.floor(Math.random() * 4) + 1];
-      console.log(cChoice);
-    });
-
+    document.getElementById("reset"); {
+      pScore.innerHTML = "Player = 15";
+      cScore.innerHTML = "Computer = 0";
+    }
+    console.log("reset");
   });
-  document.getElementById('reset').addEventListener('keydown', function (event) {
-    if (event.key === 'Enter') {
-      checkAnswer();
-    }
-  })
-runGame(checkAnswer);
-};
-
-function checkAnswer() {
-  let currentMatch = (`${pChoice} vs ${cChoice}`);
-  console.log("currentMatch");
-
-  // Draw check
-  if (pChoice === cChoice) {
-    alert(`${currentMatch} is a Draw`);
-    return;
-  }
-  // Spock
-  else if (pChoice === "Spock") {
-    if (cChoice == "Scissors" || cChoice == "Rock") {
-      alert(`${currentMatch} = You Win`);
-      pScore++;
-      console.log("Spock");
-    } else if (cChoice == "Lizzard" || cChoice == "paper") {
-      alert(`${currentMatch} = Computer Wins`);
-      cScore++;
-    }
-  }
-
-  // Rock
-  else if (pChoice === "Rock") {
-    if (cChoice == "Lizzard" || cChoice == "Scissors") {
-      alert(`${currentMatch} = You Win`);
-      pScore++;
-      console.log("Rock");
-    } else if (cChoice == "Paper" || cChoice == "Spock") {
-      alert(`${currentMatch} = Computer Wins`);
-      cScore++;
-    }
-  }
-
-  // Lizzard
-  else if (pChoice === "Lizzard") {
-    if (cChoice == "Spock" || cChoice == "Paper") {
-      alert(`${currentMatch} = You Win`);
-      pScore++;
-      console.log("Lizzard");
-    } else if (cChoice == "Scissors" || cChoice == "Rock") {
-      alert(`${currentMatch} = Computer Wins`);
-      cScore++;
-    }
-  }
-
-  // Scissors
-  else if (pChoice === "Scissors") {
-    if (cChoice == "Paper" || cChoice == "Lizzard") {
-      alert(`${currentMatch} = You Win`);
-      pScore++;
-      console.log("Scissors");
-    } else if (cChoice == "Spock" || cChoice == "Rock") {
-      alert(`${currentMatch} = Computer Wins`);
-      cScore++;
-    }
-  }
-
-  // Paper
-  else if (pChoice === "Paper") {
-    if (cChoice == "Rock" || cChoice == "Spock") {
-      alert(`${currentMatch} = You Win`);
-      pScore++;
-      console.log("Paper");
-    } else if (cChoice == "Lizzard" || cChoice == "Scissors") {
-      alert(`${currentMatch} = Computer Wins`);
-      cScore++;
-    }
-  }
-};
-
-function incrementScore() {
-  document.getElementById("pScore").textContent = pScore;
-  document.getElementById("cScore").textContent = cScore;
-};
-
-function winner() {
-  if (pScore === 4 || cScore === 4) {
-    let winner =
-      pScore === 4 ?
-      "You win the game! Congratulations!" :
-      "Computer wins the game! Try again next time!";
-    alert(winner);
-    console.log("winner");
-    return true;
-  }
-  return false;
-};
-
-reset.addEventListener("click", function () {
-
-  document.getElementById("reset"); {
-    pScore.innerHTML = "Player = 0";
-    cScore.innerHTML = "Computer = 0";
-  }
-  console.log("reset");
-});
