363d2a7e18182a3f27ce639f18712ed76a30aba8
Erik1007 <erik.hartman1007@gmail.com>
2022-07-18 07:49:55 +0000

    working on 'if' arguments for the run game/check answer function for debugging

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-let options = [
-  {
+let options = [{
     image: "lizard.webp",
     name: "Lizard",
   },
@@ -25,111 +24,114 @@ let options = [
 ];
 
 
-  function runGame() {
-    let pChoice = document.getElementsByClassName("options");
-    let options = Array.from(document.getElementsByClassName("players"));
+function runGame() {
+  let pChoice = document.getElementsByClassName("options");
+  let options = Array.from(document.getElementsByClassName("players"));
 
-    options.forEach((option) => {
-      option.addEventListener("click", function () {
-        pChoice = this.textContent;
-        console.log(pChoice)
+  options.forEach((option) => {
+    option.addEventListener("click", function () {
+      pChoice = this.textContent;
+      console.log(pChoice)
 
-        let cChoice = ["Spock", "Rock", "Lizzard", "Scissors", "Paper"];
-        cChoice = options[Math.floor(Math.random() * 4) + 1];
-        console.log(cChoice);
-        runGame(checkAnswer);
-      }); 
-      
+      let cChoice = ["Spock", "Rock", "Lizzard", "Scissors", "Paper"];
+      cChoice = options[Math.floor(Math.random() * 4) + 1];
+      console.log(cChoice);
     });
-    
-  };
 
- 
-
-  function checkAnswer() {
-   let currentMatch = (`${pChoice} vs ${cChoice}`); 
-
-      // Draw check
-      if (pChoice === cChoice) {
-        alert(`${currentMatch} is a Draw`);
-        return;
-      }
-      // Spock
-      else if (pChoice === "Spock") {
-        if (cChoice === "Scissors" || "Rock") { 
-          alert(`${currentMatch} = You Win`);
-          pScore++;
-        } else if (cChoice === "Lizzard" || "paper") {
-          alert(`${currentMatch} = Computer Wins`);
-          cScore++;
-        }
-      }
-      // Rock
-      else if (pChoice === "Rock") {
-        if (cChoice === "Lizzard" || "Scissors") {
-          alert (`${currentMatch} = You Win`);
-          pScore++;
-        } else if (cChoice === "Paper" || "Spock") {
-          alert (`${currentMatch} = Computer Wins`);
-          cScore++;
-        }
-      }
-
-      // Lizzard
-      else if (pChoice === "Lizzard") {
-        if (cChoice === "Spock" || "Paper") {
-          alert (`${currentMatch} = You Win`);
-          pScore++;
-        } else if (cChoice === "Scissors" || "Rock") { 
-          alert (`${currentMatch} = Computer Wins`);
-          cScore++;
-        }
-      }
-
-      // Scissors
-      else if (pChoice === "Scissors") {
-        if (cChoice === "Paper" || "Lizzard") {
-          alert (`${currentMatch} = You Win`);
-          pScore++;
-        } else if (cChoice === "Spock" || "Rock") {
-          alert (`${currentMatch} = Computer Wins`);
-          cScore++;
-        }
-      }
-
-      // Paper
-      else if (pChoice === "Paper") {
-        if (cChoice === "Rock" || "Spock") {
-          alert(`${currentMatch} = You Win`);
-          pScore++;
-        } else if (cChoice === "Lizzard" || "Scissors") {
-          alert(`${currentMatch} = Computer Wins`);
-          cScore++;
-        }
-      }
-    };
-  
-  function incrementScore() {
-    document.getElementById("pScore").textContent = pScore;
-    document.getElementById("cScore").textContent = cScore;
-  };
-
-  function winner() {
-    if (pScore === 4 || cScore === 4) {
-      let winner =
-        pScore === 4
-          ? "You win the game! Congratulations!"
-          : "Computer wins the game! Try again next time!";
-      alert(winner);
-      return true;
-    }
-    return false;
-  };
-
-    reset.addEventListener("click", function () {  
-
-      document.getElementById("reset"); {
-        pScore.innerHTML = "Player = 0";
-        cScore.innerHTML = "Computer = 0";
-      }
   });
+  document.getElementById('reset').addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+      checkAnswer();
+    }
+  })
+runGame(checkAnswer);
+};
+
+function checkAnswer() {
+  let currentMatch = (`${pChoice} vs ${cChoice}`);
+
+  // Draw check
+  if (pChoice === cChoice) {
+    alert(`${currentMatch} is a Draw`);
+    return;
+  }
+  // Spock
+  else if (pChoice === "Spock") {
+    if (cChoice == "Scissors" || cChoice == "Rock") {
+      alert(`${currentMatch} = You Win`);
+      pScore++;
+    } else if (cChoice == "Lizzard" || cChoice == "paper") {
+      alert(`${currentMatch} = Computer Wins`);
+      cScore++;
+    }
+  }
+
+  // Rock
+  else if (pChoice === "Rock") {
+    if (cChoice == "Lizzard" || cChoice == "Scissors") {
+      alert(`${currentMatch} = You Win`);
+      pScore++;
+    } else if (cChoice == "Paper" || cChoice == "Spock") {
+      alert(`${currentMatch} = Computer Wins`);
+      cScore++;
+    }
+  }
+
+  // Lizzard
+  else if (pChoice === "Lizzard") {
+    if (cChoice == "Spock" || cChoice == "Paper") {
+      alert(`${currentMatch} = You Win`);
+      pScore++;
+    } else if (cChoice == "Scissors" || cChoice == "Rock") {
+      alert(`${currentMatch} = Computer Wins`);
+      cScore++;
+    }
+  }
+
+  // Scissors
+  else if (pChoice === "Scissors") {
+    if (cChoice == "Paper" || cChoice == "Lizzard") {
+      alert(`${currentMatch} = You Win`);
+      pScore++;
+    } else if (cChoice == "Spock" || cChoice == "Rock") {
+      alert(`${currentMatch} = Computer Wins`);
+      cScore++;
+    }
+  }
+
+  // Paper
+  else if (pChoice === "Paper") {
+    if (cChoice == "Rock" || cChoice == "Spock") {
+      alert(`${currentMatch} = You Win`);
+      pScore++;
+    } else if (cChoice == "Lizzard" || cChoice == "Scissors") {
+      alert(`${currentMatch} = Computer Wins`);
+      cScore++;
+    }
+  }
+};
+
+function incrementScore() {
+  document.getElementById("pScore").textContent = pScore;
+  document.getElementById("cScore").textContent = cScore;
+};
+
+function winner() {
+  if (pScore === 4 || cScore === 4) {
+    let winner =
+      pScore === 4 ?
+      "You win the game! Congratulations!" :
+      "Computer wins the game! Try again next time!";
+    alert(winner);
+    return true;
+  }
+  return false;
+};
+
+reset.addEventListener("click", function () {
+
+  document.getElementById("reset"); {
+    pScore.innerHTML = "Player = 0";
+    cScore.innerHTML = "Computer = 0";
+  }
+});
